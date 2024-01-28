1606110e6c79e30c717dcf7ed7d9fe8c6dd047d1
eoe789 <eduardoo.enriquez789@gmail.com>
2024-01-28 06:19:28 -0600

    Think this is as good as we're gonna get

--- src/app/homepage/page.js
+++ src/app/homepage/page.js
@@ -69,14 +69,17 @@ const Show = () => {
     }
 
     return (
-        <main style={{dislpay: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <button onClick={handleClick}>
-                +
+        <main style={{justifyContent: 'center', alignItems: 'center', backgroundColor:'#ffd099'}}>
+            <h1 style={{textAlign:'center', backgroundColor: '#ffc175', fontSize: '50px', color: 'white', width: 'auto', marginBottom: '7px'}}>
+                Home
+            </h1>
+            <button onClick={handleClick} style={{display:'flex', justifyContent: 'center', alignItems:'center', fontSize:'20px', backgroundColor:'black', borderRadius:'50px', height:'25px', width:'25px', margin:'0px 0px'}}>
+                    +
             </button>
             <br></br>
-            <div style={{backgroundColor:'PeachPuff', height:'100vh', width:'auto'}}>
+            <div style={{backgroundColor:'PeachPuff', height:'100vh', width:'auto', margin:'0px 250px', borderRadius: '20px', padding:'10px 0px'}}>
                 {projects.map((project) => 
-                    <div key={project.id} className="text-5xl font-bold font-sans_serif text-gray-700 items-center flex justify-center" style={{display: 'flex', justifyContent: 'space-between', padding: '10px 20px', borderRadius: '50px',backgroundColor: 'white',color: 'dark-gray-700',textAlign: 'center', margin: '10px 0'}}>
+                    <div key={project.id} className="text-5xl font-bold font-sans_serif text-gray-700 items-center flex justify-center" style={{display: 'flex', justifyContent: 'space-between', padding: '10px 20px', borderRadius: '50px',backgroundColor: 'white',color: 'dark-gray-700',textAlign: 'center', margin: '10px 50px'}}>
                         <div style={{width: 'fit', display: 'flex', justifyContent: 'center'}}>
                             <EditText text={project.text} onTextChange={newText => handleTextChange(project.id, newText)} />
                         </div>
@@ -86,6 +89,11 @@ const Show = () => {
                     </div>
                 )}
             </div>
+            <footer>
+                <div style={{textAlign:'center', backgroundColor:'#ffc175', fontSize:'50px', color:'white', width:'auto', margin:'10px 0px'}}>
+                    Footer
+                </div>
+            </footer>
         </main>
     );
 }
